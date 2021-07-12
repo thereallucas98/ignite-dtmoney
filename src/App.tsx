967fd8630c283b2e28a80b4d382d3d1f17453b9b
thereallucas98 <lucas1998david@gmail.com>
2021-07-12 20:23:19 -0300

    Criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257E6;
-`;
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title className="title">Hello World</Title>
+      <h1>Hello World!</h1>
+      <GlobalStyle />
     </div>
   );
 }
