fc1797cc0875ea3e5c0a75b422c640725d4ba35a
thereallucas98 <lucas1998david@gmail.com>
2021-07-12 20:09:22 -0300

    Instalando Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257E6;
+`;
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title className="title">Hello World</Title>
     </div>
   );
 }
